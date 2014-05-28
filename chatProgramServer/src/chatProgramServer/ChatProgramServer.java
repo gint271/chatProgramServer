277bc437717f0b6aa8dce168b3bf2b3c7894ac51
Ryan Milem <gint271@gmail.com>
2014-05-28 11:25:13 -0700

    Initial commit.

---
+++ chatProgramServer/src/chatProgramServer/ChatProgramServer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package chatProgramServer;
+
+/**
+ * @author r.milem
+ *
+ */
+public class ChatProgramServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
