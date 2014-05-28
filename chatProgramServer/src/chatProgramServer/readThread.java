87e684203b42b144777d4b620fa5112b2d641120
Ryan Milem <gint271@gmail.com>
2014-05-28 16:17:37 -0700

    Two way communication works with putty.

--- chatProgramServer/src/chatProgramServer/readThread.java
+++ chatProgramServer/src/chatProgramServer/readThread.java
@@ -1,5 +1,9 @@
 package chatProgramServer;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 public class readThread implements Runnable
@@ -13,6 +17,30 @@ public class readThread implements Runnable
 	
 	public void run()
 	{
+		BufferedReader reader = null;
 		
+		try
+		{
+			reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			
+		}
+		catch (Exception e)
+		{
+			System.out.println("Failed to make reader.");
+			System.exit(2);
+		}
+		
+		while(true)
+		{
+			try
+			{
+				System.out.println(reader.readLine());
+			}
+			catch (Exception e)
+			{
+				System.out.println("Failed to get text from socket.");
+				System.exit(2);
+			}
+		}
 	}
 }
